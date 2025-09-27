0f46f33a2bc1c255ee5448051a76b94d409d22c1
amin-baghiyev <baghiyev.amin@gmail.com>
2025-09-27 11:31:11 +0000

    Depth - NULL tree bug fixed

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -11,6 +11,8 @@ size_t binary_tree_depth(const binary_tree_t *tree)
 {
 	size_t depth;
 
+	if (tree == NULL)
+		return (0);
 	for (depth = 0; tree->parent != NULL; depth++)
 		tree = tree->parent;
 	return (depth);
